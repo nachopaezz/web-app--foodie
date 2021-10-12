d5aa111c9ccff084d5ab642b72981501c6ad8ee8
Nacho <ignaciopaez96@gmail.com>
2021-10-12 11:19:55 -0300

    test1 creo q bien

--- api/tests/routes/country.spec.js
+++ api/tests/routes/country.spec.js
@@ -6,7 +6,12 @@ const { Recipe, conn } = require('../../src/db.js');
 
 const agent = session(app);
 const recipe = {
-  name: 'Milanea a la napolitana',
+  id:"e4efaec8-ea77-11eb-9a03-0242ac130003",
+  title: "Milanesa a la napolitana",
+  summary: "Milanesa recubierta con queso y salsa",
+  spoonacularScore: 100,
+  healthScore: 40,
+  instructions: "Fritar"
 };
 
 describe('Recipe routes', () => {
